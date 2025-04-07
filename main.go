de691d8b2e558cd8e9032a0ff3f203b5296a2d8c
Raghav Pandit <panditraghav18@gmail.com>
2025-04-07 17:17:34 +0530

    If else statement

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello World")
+  // Basic
+  if 7%2 == 0 {
+    fmt.Println("7 is even")
+  } else {
+    fmt.Println("7 is odd")
+  }
+
+  // A statement can precede conditionals; any variables declared in
+  // this statement are available in the current and all subsequent branches.
+  if num := 9; num < 0 {
+    fmt.Println(num, "is negative")
+  } else if num < 10 {
+    fmt.Println(num, "has 1 digit")
+  } else {
+    fmt.Println(num, "has multiple digits")
+  }
 }
