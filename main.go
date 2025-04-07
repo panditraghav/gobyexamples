d7e0347a85daf4f8d608588bacde71b771a00186
Raghav Pandit <panditraghav18@gmail.com>
2025-04-07 15:24:04 +0530

    Variables

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello World")
+  var a int = 10
+  fmt.Println("a: ", a)
+  fmt.Printf("Typeof a: %T\n", a)
+
+  var b, c int = 10, 20
+  fmt.Println("b: ", b, ", c: ", c)
+  fmt.Printf("Typeof b: %T\n", b)
+
+  var d int
+  fmt.Println("d: ", d)
+
+  f := "apple"
+  fmt.Println(f)
 }
