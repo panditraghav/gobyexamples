d05ec0209ee78fdec9b854a275abd858a551f8a1
Raghav Pandit <panditraghav18@gmail.com>
2025-04-07 15:37:10 +0530

    For loops

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import (
-  "fmt"
-)
+import "fmt"
 
 func main() {
-  fmt.Println("Hello World")
+  i := 1
+  // Only condition
+  for i <= 3 {
+    fmt.Println(i)
+    i = i + 1
+  }
+
+  // Standard loop
+  for j := 0; j < 3; j++ {
+    fmt.Println(j)
+  }
+
+  // 0 1 2
+  for i := range 3 {
+    fmt.Println("range", i)
+  }
+
+  // Infinite loop
+  for {
+    fmt.Println("loop")
+    break
+  }
+
+  for n := range 6 {
+    if n%2 == 0 {
+      continue
+    }
+    fmt.Println(n)
+  }
 }
