13daccdf31c0754235232ae6f5d6ef858761c893
Raghav Pandit <panditraghav18@gmail.com>
2025-04-08 13:35:03 +0530

    Multiple return values

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-  "fmt"
-)
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 7
+}
 
 func main() {
-  fmt.Println("Hello World")
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
 }
