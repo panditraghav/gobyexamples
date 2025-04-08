4e3aab582d456bb223660273b0f1dc4aae5a4103
Raghav Pandit <panditraghav18@gmail.com>
2025-04-08 13:50:36 +0530

    Variadic functions

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
-func vals() (int, int) {
-	return 3, 7
+func sums(nums ...int) {
+	fmt.Printf("Type of arg: %T\n", nums)
+	fmt.Println("nums: ", nums)
+	var total int = 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println("total: ", total)
 }
 
 func main() {
-	a, b := vals()
-	fmt.Println(a)
-	fmt.Println(b)
+	sums(1, 2)
+	sums(4, 5, 6, 7)
 
-	_, c := vals()
-	fmt.Println(c)
+	// If you already have multiple args in a slice,
+	// apply them to a variadic function using func(slice...) like this.
+	nums := []int{8, 9, 10, 11}
+	sums(nums...)
 }
