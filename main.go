74b0fd520220f23b1bf442ac13d97e2c31fa0296
Raghav Pandit <panditraghav18@gmail.com>
2025-04-24 16:08:24 +0530

    Interfaces

--- main.go
+++ main.go
@@ -2,38 +2,66 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"math"
 )
 
-func main() {
-	const s = "สวัสดีt"
-	fmt.Println("len(s): ", len(s))
-
-	for i := 0; i < len(s); i++ {
-		fmt.Printf("%x ", s[i])
-	}
-	fmt.Println()
-
-	fmt.Println("Rune count:", utf8.RuneCountInString(s))
-
-	for idx, runeValue := range s {
-		fmt.Printf("%#U starts at %d\n", runeValue, idx)
-	}
-
-	fmt.Println("\nUsing DecodeRuneInString")
-	for i, w := 0, 0; i < len(s); i += w {
-		runeValue, width := utf8.DecodeRuneInString(s[i:])
-		fmt.Printf("%#U starts at %d\n", runeValue, i)
-		w = width
-		examineRune(runeValue)
-	}
-	// Values enclosed in single quotes are rune literals. We can compare a rune value to a rune literal directly.
+type geometry interface {
+	area() float64
+	perimeter() float64
 }
 
-func examineRune(r rune) {
-	if r == 't' {
-		fmt.Println("found tee")
-	} else if r == 'ส' {
-		fmt.Println("found so sua")
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func (r rect) perimeter() float64 {
+	return 2*r.height + r.width
+}
+
+func (c circle) area() float64 {
+	return c.radius * c.radius
+}
+
+func (c circle) perimeter() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println("Area: ", g.area())
+	fmt.Println("Perimeter: ", g.perimeter())
+}
+
+func detectCircle(g geometry) {
+	if c, ok := g.(circle); ok {
+		fmt.Println("Circle with radius: ", c.radius)
 	}
+}
+
+func geometrySwitch(g geometry) {
+	switch g.(type) {
+	case circle:
+		fmt.Println("Circle with radius: ", g.(circle).radius)
+	case rect:
+		fmt.Println("Rectangle with width: ", g.(rect).width, ", height: ", g.(rect).height)
+	default:
+		fmt.Println("Not a geometry!")
+	}
+}
+
+func main() {
+	var r rect = rect{width: 3, height: 4}
+	c := circle{radius: 4}
+	measure(r)
+	measure(c)
+	detectCircle(c)
+	geometrySwitch(r)
+	geometrySwitch(c)
 }
