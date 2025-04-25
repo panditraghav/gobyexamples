56030059b89ebedf23eb0b41fdd0e4fe24499cf7
Raghav Pandit <panditraghav18@gmail.com>
2025-04-25 19:32:45 +0530

    Custom Errors

--- main.go
+++ main.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 )
 
+type argError struct {
+	arg     int
+	message string
+}
+
+// Adding this Error method makes argError implement the error interface.
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.message)
+}
+
 func f(arg int) (int, error) {
 	if arg == 42 {
-		return -1, errors.New("Can't work with 42")
+		// Return our custom error.
+		return -1, &argError{arg, "Can't work with it"}
 	}
 	return arg + 3, nil
 }
@@ -55,5 +66,20 @@ func main() {
 			continue
 		}
 		fmt.Printf("Tea is ready! (i = %v)\n", i)
+	}
+
+	_, err := f(42)
+	var ae *argError
+	/*
+		errors.As is a more advanced version of errors.Is.
+		It checks that a given error (or any error in its chain) matches
+		a specific error type and converts to a value of that type,
+		returning true. If there’s no match, it returns false.
+	*/
+	if errors.As(err, &ae) {
+		fmt.Println(ae.arg)
+		fmt.Println(ae.message)
+	} else {
+		fmt.Println("err doesn't match argError")
 	}
 }
